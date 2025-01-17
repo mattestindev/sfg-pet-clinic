451e54cb28778474dc9d62d7a1dffea33053d14a
Matt Estin <mattestin@gmail.com>
2025-01-17 09:45:29 -0500

    Initial Project Add

---
+++ src/main/java/me/springframework/sfgpetclinicu/SfgPetClinicuApplication.java
@@ -0,0 +1,13 @@
+package me.springframework.sfgpetclinicu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinicuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/springframework/sfgpetclinicu/SfgPetClinicuApplicationTests.java
@@ -0,0 +1,13 @@
+package me.springframework.sfgpetclinicu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
